02eb39f40e90be00e7a6ac0a492c4feb429fa7bf
Илья <86857441+Hryhor@users.noreply.github.com>
2022-08-31 23:09:03 +0300

    Add files via upload

---
+++ Coffee/gulpfile.js
@@ -0,0 +1,94 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+
+/*автоматически обновляем страницу*/
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: 'app/'
+        }
+    });
+}
+
+function cleanDist() {
+    return del('dist')
+}
+
+/*сжимаем картинки*/
+function images() {
+    return src('app/img/**/*')
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle({interlaced: true}),
+                imagemin.mozjpeg({quality: 75, progressive: true}),
+                imagemin.optipng({optimizationLevel: 5}),
+                imagemin.svgo({
+                    plugins: [
+                        {removeViewBox: true},
+                        {cleanupIDs: false}
+                    ]
+                })
+            ]
+        ))
+        .pipe(dest('dist/img'))
+}
+
+/*подключение скриптов*/
+function scripts() {
+    return src([
+        'node_modules/jquery/dist/jquery.js',
+        'app/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
+
+/*подключение стилей, конвертация scss в css и сжатие*/
+function styles() {
+    return src('app/scss/style.scss')
+        .pipe(scss({outputStyle:  'compressed'}))
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version'],
+            grid: true
+        }))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
+}
+
+function build() {
+    return src([
+        'app/css/style.min.css',
+        'app/fonts/**/*',
+        'app/js/main.min.js',
+        'app/*.html'
+
+    ], {base: 'app'})
+        .pipe(dest('dist'))
+}
+
+/*автоматическая конвертация*/
+function watching() {
+    watch(['app/scss/**/*.scss'], styles)
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
+    watch(['app/*.html']).on('change', browserSync.reload);
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = cleanDist;
+
+exports.build = series(cleanDist, images, build)
+exports.default = parallel(styles, scripts, browsersync, watching);
